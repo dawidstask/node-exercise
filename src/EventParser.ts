90cb804501f2bf8f566e429f1a55f2283b327ffe
Dawid <dawstask@gmail.com>
2023-12-30 20:05:33 +0100

    wip: formatScore method

--- src/EventParser.ts
+++ src/EventParser.ts
@@ -1,5 +1,6 @@
 import type { Match } from "./types/Match.ts";
 import { Sport } from "./types/Sport.ts";
+import matches from "./matches.ts";
 
 export default class EventParser {
   getSeparator(match: Match): string | null {
@@ -25,36 +26,33 @@ export default class EventParser {
   }
 
   formatScore(match: Match): string | null {
-    if (match.sport === Sport.SOCCER) {
+    if (
+      (match.sport === Sport.SOCCER || match.sport === Sport.HANDBALL) &&
+      typeof match.score === 'string'
+    ) {
       return match.score;
-    } else if (match.sport === 'tennis') {
-      var scores = /([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+)/.exec(match.score);
-      var set1 = scores[2];
-      var set2 = scores[3];
-      var set3 = scores[4];
-
-      return "Main score: " + scores[1] + " ("
-        + "set1 " + set1 + ", "
-        + "set2 " + set2 + ", "
-        + "set3 " + set3
-        + ")";
-    } else if (match.sport === 'volleyball') {
-      var scores = /([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+)/.exec(match.score);
-      var set1 = scores[2];
-      var set2 = scores[3];
-      var set3 = scores[4];
-
-      return "Main score: " + scores[1] + " ("
-        + "set1 " + set1 + ", "
-        + "set2 " + set2 + ", "
-        + "set3 " + set3
-        + ")";
-    } else if (match.sport === 'basketball') {
-      return match.score[0][0] + ',' + match.score[0][1] + ',' + match.score[1][0] + ',' + match.score[1][1];
-    } else if (match.sport === 'handball') {
-      return match.score;
-    } else {
-      return null;
     }
+
+    if (
+      (match.sport === Sport.TENNIS || match.sport === Sport.VOLLEYBALL) &&
+      typeof match.score === 'string'
+    ) {
+      const scores = match.score.split(',')
+      const set1 = scores[1];
+      const set2 = scores[2];
+      const set3 = scores[3];
+
+      return "Main score: " + scores[0] + " ("
+        + "set1 " + set1 + ", "
+        + "set2 " + set2 + ", "
+        + "set3 " + set3
+        + ")";
+    }
+
+    if (match.sport === Sport.BASKETBALL && typeof match.score === 'object') {
+      return match.score.flat().join(',');
+    }
+
+    return null;
   }
 }
